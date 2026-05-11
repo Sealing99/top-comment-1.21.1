00cf3410fc5a72d36e91aebda1302542835783ce
Sealing99 <bramelientje1@gmail.com>
2026-05-11 17:51:39 +0200

    Initial commit

---
+++ src/main/java/net/sealing99/topcomment/TopCommentModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.sealing99.topcomment;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TopCommentModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
