d2b73f72cd5aff38647a15fe00152459894c19a0
Sealing99 <bramelientje1@gmail.com>
2026-05-12 18:40:11 +0200

    Init

---
+++ src/main/java/net/sealing99/topcomment/TopCommentModClient.java
@@ -0,0 +1,10 @@
+package net.sealing99.topcomment;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TopCommentModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
